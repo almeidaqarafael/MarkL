7f9654df4f19299aec2438b5b797d14e32722f07
almeidaqarafael <rafaeltesterqa@gmail.com>
2024-06-28 16:05:04 -0300

    removendo faker.js e adicionando arquivo markL.json

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,14 +1,12 @@
 /// <reference types="Cypress" /> 
 
-import { faker } from '@faker-js/faker'
-
 describe('tarefas', () => {
 
   it('deve cadastrar uma nova tarefa', () => {
     cy.visit('http://localhost:3000')
 
     cy.get('input[placeholder="Add a new Task"]')
-      .type(faker.music.songName())
+      .type('Comprar uma codorna')
 
     cy.contains('button', 'Create')
       .click()
