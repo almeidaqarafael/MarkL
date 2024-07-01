3941489d0382d75ccfef92566a04588afb5a97ec
almeidaqarafael <rafaeltesterqa@gmail.com>
2024-07-01 09:59:16 -0300

    Limpando o cenário de teste e adicionando um item

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -3,6 +3,15 @@
 describe('tarefas', () => {
 
   it('deve cadastrar uma nova tarefa', () => {
+
+    cy.request({ 
+      url: 'http://localhost:3333/helper/tasks',
+      method: 'DELETE',
+      body: { name: 'Comprar uma codorna' }
+    }).then(res => {
+        expect(res.status).to.eq(204)
+    });
+    
     cy.visit('http://localhost:3000')
 
     cy.get('input[placeholder="Add a new Task"]')
@@ -10,6 +19,9 @@ describe('tarefas', () => {
 
     cy.contains('button', 'Create')
       .click()
+
+    cy.contains('main div p', 'Comprar uma codorna')
+      .should('be.visible')
   });
 
 });
