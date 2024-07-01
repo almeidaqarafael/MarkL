fa2114f87b5c7f49cd6e4843f05de96083a907fa
almeidaqarafael <rafaeltesterqa@gmail.com>
2024-07-01 11:48:33 -0300

    Não deve permitir tarefa duplicada

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -24,4 +24,38 @@ describe('tarefas', () => {
       .should('be.visible')
   });
 
+  it('não deve permitir tarefa duplicada', () => {
+
+    cy.request({ 
+      url: 'http://localhost:3333/helper/tasks',
+      method: 'DELETE',
+      body: { name: 'Comprar uma casa para a codorna' }
+    }).then(res => {
+        expect(res.status).to.eq(204)
+    });
+
+    cy.request({ 
+      url: 'http://localhost:3333/tasks',
+      method: 'POST',
+      body: { 
+        name: 'Comprar uma casa para a codorna',
+        is_done: false
+       }
+    }).then(res => {
+      expect(res.status).to.eq(201)
+  });
+
+    cy.visit('http://localhost:3000')
+
+    cy.get('input[placeholder="Add a new Task"]')
+      .type('Comprar uma casa para a codorna')
+
+    cy.contains('button', 'Create')
+      .click()
+
+    cy.get('.swal2-html-container')
+      .should('be.visible')
+      .should('have.text', 'Task already exists!')
+  });
+
 });
